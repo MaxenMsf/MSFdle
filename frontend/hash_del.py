2872a5c4a4fc0db9a81743aa56f89b876046c5af
MaxenMSF <alexandremione@gmail.com>
2025-09-18 11:11:36 +0200

    :sparkles: Ajout de la quasi totalité des capacités des personnages avec un script pour enlever les HASH dans le nom des fichiers

---
+++ frontend/hash_del.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+# Dossier contenant tes icônes téléchargées
+ICON_DIR = "capacites"
+
+# Regex pour enlever le hash (8 caractères hexadécimaux) avant .png
+HASH_RE = re.compile(r'_[0-9a-f]{8}(?=\.png)')
+
+for filename in os.listdir(ICON_DIR):
+    if filename.endswith(".png"):
+        new_name = HASH_RE.sub('', filename)
+        if new_name != filename:
+            old_path = os.path.join(ICON_DIR, filename)
+            new_path = os.path.join(ICON_DIR, new_name)
+            os.rename(old_path, new_path)
+            print(f"Renommé : {filename} -> {new_name}")
+
+print("Tous les fichiers ont été renommés !")
